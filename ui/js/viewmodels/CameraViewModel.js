8ee3fdf3c8b238db404582fbbbed109823ab3951
Eduardo Tachotte <etachott@c1r6p4.42sp.org.br>
2024-09-12 22:47:15 -0300

    Add snapshot feature to camera

--- ui/js/viewmodels/CameraViewModel.js
+++ ui/js/viewmodels/CameraViewModel.js
@@ -5,11 +5,26 @@ class CameraViewModel {
     constructor() {
         this.cameraModel = new CameraModel();
         this.cameraView = new CameraView();
+
+        this.cameraView.bindCapture(this.handleCapture.bind(this));
     }
 
     async initialize() {
         const stream = await this.cameraModel.initializeCamera();
         this.cameraView.bindStream(stream);
+    }
+
+    handleCapture() {
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+        canvas.width = this.cameraView.videoElement.clientWidth;
+        canvas.height = this.cameraView.videoElement.clientHeight;
+        context.drawImage(this.cameraView.videoElement, 0, 0, canvas.width, canvas.height);
+        const image = canvas.toDataURL('image/png');
+
+        this.cameraView.videoElement.style.display = 'none';
+        this.cameraView.imageArea.innerHTML += `<img src="${image}" alt="Captured Image" />`;
+        // this.cameraModel.saveImage(image); later
     }
 }
 
--- ui/js/views/CameraView.js
+++ ui/js/views/CameraView.js
@@ -1,10 +1,16 @@
 class CameraView {
     constructor() {
         this.videoElement = document.getElementById('user-camera');
+        this.cameraButton = document.getElementById('camera-button');
+        this.imageArea = document.getElementById('drop-area');
     }
 
     bindStream(stream) {
         this.videoElement.srcObject = stream;
+    }
+
+    bindCapture(handler) {
+        this.cameraButton.addEventListener('click', handler);
     }
 }
 
