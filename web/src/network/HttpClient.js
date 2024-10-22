d1f746c2659eceafb996aad3df808fe5fbfb9df4
Eduardo Tachotte <etachott@c1r6p4.42sp.org.br>
2024-10-21 22:52:59 -0300

    Add custom http client

---
+++ web/src/network/HttpClient.js
@@ -0,0 +1,41 @@
+class HttpClient {
+    constructor(baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+	async get(endpoint) {
+		try {
+			const url = `${this.baseUrl}${endpoint}`;
+			const response = await fetch(url);
+			const checkedResponse = await this._checkStatus(response);
+			return checkedResponse.json();
+		} catch (error) {
+			return this._handleError(error);
+		}
+	}
+
+	async post(endpoint, data) {
+		try {
+			const url = `${this.baseUrl}${endpoint}`;
+			const response = await fetch(url, { method: 'POST', body: JSON.stringify(data) });
+			const checkedResponse = await this._checkStatus(response);
+			return checkedResponse.json();
+		} catch (error) {
+			return this._handleError(error);
+		}
+	}
+
+	_checkStatus(response) {
+		if (!response.ok) {
+			throw new Error(response.statusText);
+		}
+		return response;
+	}
+
+	_handleError(error) {
+		console.error(error);
+		throw error;
+	}
+}
+
+export default HttpClient;
