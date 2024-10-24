a47e5ae0cec3ab832e4ed07ad69097007240ef74
Eduardo Tachotte <etachott@c1r6p4.42sp.org.br>
2024-10-23 22:30:57 -0300

    Revamp routing logic with PHP witchery

--- api/core/Router.php
+++ api/core/Router.php
@@ -2,24 +2,42 @@
 
 class Router
 {
+	private $routes = [
+		'GET' => [
+			'#^/home$#' => ['controller' => 'HomeController', 'method' => 'handleRequest'],
+			'#^/$#' => ['controller' => 'HomeController', 'method' => 'handleRequest'],
+			'#^/images/(\w+)$#' => ['controller' => 'ImagesController', 'method' => 'handleGetByIdRequest'],
+			'#^/images$#' => ['controller' => 'ImagesController', 'method' => 'handleGetRequest'],
+		],
+		'POST' => [
+			'#^/images$#' => ['controller' => 'ImagesController', 'method' => 'handlePostRequest'],
+		],
+	];
+
 	public function route()
 	{
 		$requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+		$requestMethod = $_SERVER['REQUEST_METHOD'];
 
-		switch ($requestUri) {
-			case '/home':
-			case '/':
-				$homeController = new HomeController();
-				$homeController->handleRequest();
-				break;
-			case '/images':
-				$imageController = new ImagesController();
-				$imageController->handleRequest();
-				break;
-			default:
-				header("HTTP/1.1 404 Not Found");
-				echo json_encode(array("error" => "Not found"));
-				break;
+		if (!isset($this->routes[$requestMethod])) {
+			header("HTTP/1.1 405 Method Not Allowed");
+			echo json_encode(array("error" => "Method not allowed"));
+			return;
+		}
+
+		foreach ($this->routes[$requestMethod] as $pattern => $route) {
+			if (preg_match($pattern, $requestUri, $matches)) {
+				$controllerName = $route['controller'];
+				$method = $route['method'];
+
+				if (class_exists($controllerName)) {
+					$controller = new $controllerName();
+					array_shift($matches);
+
+					call_user_func_array([$controller, $method], $matches);
+					return;
+				}
+			}
 		}
 	}
 }
