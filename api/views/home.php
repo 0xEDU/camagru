cd3fb1f107f4b0cad76fc78cf0ee2f1f6739d0ac
Eduardo Tachotte <etachott@c1r6p4.42sp.org.br>
2024-10-27 15:39:28 -0300

    Remove placeholder code and add button

--- api/views/home.php
+++ api/views/home.php
@@ -3,20 +3,14 @@
 	<div id="drop-area" class="relative m-3 flex flex-col items-center justify-center">
 		<video id="user-camera" class="w-9/12 rounded-xl shadow" playsinline autoplay muted></video>
 		<div class="absolute inset-0 flex items-end justify-center">
-			<button id="camera-button" class="w-16 h-16 mb-2 rounded-full bg-color-tertiary">
+			<button id="camera-button-snap" class="w-16 h-16 mb-2 rounded-full bg-color-tertiary">
+			</button>
+			<button id="camera-button-retry" class="w-16 h-16 mb-2 rounded-full bg-color-tertiary">
 			</button>
 		</div>
 	</div>
 </section>
 <aside class="bg-color-primary text-color-primary flex-shrink-0 w-1/3">
 	<section id="last-taken-pics-gallery" class="h-full w-full p-6">
-		<div class="w-full flex">
-			<img id="gallery-image" class="w-full h-[18vh] p-2 object-scale-down" src="./assets/orange-cat.png" alt="">
-			<img id="gallery-image" class="w-full h-[18vh] p-2 object-scale-down" src="./assets/orange-cat.png" alt="">
-		</div>
-		<div class="w-full flex">
-			<img id="gallery-image" class="w-full h-[18vh] p-2 object-scale-down" src="./assets/orange-cat.png" alt="">
-			<div class="w-full h-[18vh] p-2"></div>
-		</div>
 	</section>
 </aside>
