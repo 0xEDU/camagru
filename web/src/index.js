9d70fef016123624e1b0a49787eb59b286453fc6
edu <bfwaend@gmail.com>
2024-10-30 21:20:41 -0300

    Set base url to nginx api endpoint

--- web/src/index.js
+++ web/src/index.js
@@ -4,7 +4,7 @@ import HomeViewModel from './ui/viewmodels/HomeViewModel.js';
 import HttpClient from './network/HttpClient.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
-	const baseUrl = 'http://camagru-api:8042/api';
+	const baseUrl = 'http://localhost:8080/api';
 	const httpClient = new HttpClient(baseUrl);
 
 	const homeViewModel = new HomeViewModel(httpClient);
