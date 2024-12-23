338a1a295ccb7dacad41843fb89dafd321cb2b0e
Eduardo Tachotte <etachott@c1r6p4.42sp.org.br>
2024-12-23 17:38:48 -0300

    Remove unused imports

--- web/src/index.js
+++ web/src/index.js
@@ -1,9 +1,5 @@
-import CameraComponent from './components/CameraComponent.js';
-import CameraService from './services/CameraService.js';
-import DragDropService from './services/DragDropService.js';
 import HttpClient from './network/HttpClient.js';
 import { Router } from './router/Router.js';
-import DragDropComponent from './components/DragDropComponent.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
 	const baseUrl = 'http://localhost:8042';
