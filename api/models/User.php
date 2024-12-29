43f3bc7143f42d2a7cfe5319f2e8ae7012021cc8
edu <bfwaend@gmail.com>
2024-12-29 05:57:02 -0300

    Update user model to use email as pkey

--- api/models/User.php
+++ api/models/User.php
@@ -1,13 +1,11 @@
 <?php
 
 class User {
-    public $id;       // Primary key
     public $name;     // User name
     public $email;    // User email
     public $password; // User password (hashed)
 
-    public function __construct($name, $email, $password, $id = null) {
-        $this->id = $id;
+    public function __construct($name, $email, $password) {
         $this->name = $name;
         $this->email = $email;
         $this->password = $password;
--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+    password_hash VARCHAR(255) NOT NULL
 );
