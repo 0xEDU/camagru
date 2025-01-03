6cd9c1552d6c6e30b21c17a97bbba5ae48d97d1a
edu <bfwaend@gmail.com>
2025-01-03 15:24:19 -0300

    Update User model

--- api/models/User.php
+++ api/models/User.php
@@ -1,15 +1,19 @@
 <?php
 
 class User {
-    public $id;       // User ID
-    public $username; // User name
-    public $email;    // User email
-    public $password; // User password (hashed)
+    public $id;        // User ID
+    public $username;  // User name
+    public $email;     // User email
+    public $password;  // User password (hashed)
+    public $token;     // Registration token
+    public $is_active; // User is active
 
-    public function __construct($username, $email, $password, $id = null) {
+    public function __construct($username, $email, $password, $id = null, $token = null, $is_active = 0) {
         $this->id = $id;
         $this->username = $username;
         $this->email = $email;
         $this->password = $password;
+        $this->token = $token;
+        $this->is_active = $is_active;
     }
 }
