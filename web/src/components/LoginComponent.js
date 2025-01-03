9ca5cae1aeba061cfcaf78c0b17f80b328fcfb78
edu <bfwaend@gmail.com>
2025-01-03 15:29:46 -0300

    Add login component and service

--- web/src/components/LoginComponent.js
+++ web/src/components/LoginComponent.js
@@ -1,21 +1,52 @@
+export default class LoginComponent {
+	constructor(loginService) {
+		this.loginService = loginService;
 
-export default class RegisterComponent {
-	constructor(registerService) {
-		this.registerService = registerService;
-
-		this.registerForm = document.getElementById('login-form');
+		this.loginForm = document.getElementById('login-form');
 		this.formMessage = document.getElementById('form-message');
 	}
 
     async initialize() {
-        this.registerForm.addEventListener('submit', this._handleLogin.bind(this));
+        this.loginForm.addEventListener('submit', this._handleLogin.bind(this));
     }
 
     destroy() {
-        this.registerForm.removeEventListener('submit', this._handleLogin.bind(this));
+        this.loginForm.removeEventListener('submit', this._handleLogin.bind(this));
     }
 
-    _handleLogin(event) {
+    async _handleLogin(event) {
         event.preventDefault();
+
+		const username = document.getElementById('login-username').value.trim();
+		const password = document.getElementById('login-password').value;
+
+        const userData = {
+            username,
+            password
+        };
+
+        const response = await this.loginService.postLogin(userData);
+		if (response instanceof Error) {
+			this._displayErrorMessage([response.message]);
+			return;
+		}
+
+		this.loginForm.reset();
+
+		this._displaySuccessMessage();
     }
+
+	_displaySuccessMessage() {
+		this.formMessage.innerHTML = 'Logged in successfully!';
+		this.formMessage.style.color = 'green';
+	}
+
+	_displayErrorMessage(errors) {
+		this.formMessage.innerHTML = errors.map(error => `<p>${error}</p>`).join('');
+		this.formMessage.style.color = 'red';
+	}
+
+	_clearErrorMessage() {
+		this.formMessage.innerHTML = '';
+	}
 }
--- web/src/services/LoginService.js
+++ web/src/services/LoginService.js
@@ -2,4 +2,13 @@ export default class LoginService {
 	constructor(httpClient) {
 		this.httpClient = httpClient
 	}
+
+	async postLogin(userData) {
+		try {
+			const response = await this.httpClient.post('/login', userData);
+			return response;
+		} catch (error) {
+			return error;
+		}
+	}
 }
