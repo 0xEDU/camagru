7e7b9de33b9908a472d8f204e709459a0d6b0111
edu <bfwaend@gmail.com>
2025-02-10 23:31:29 -0300

    Move I forgot my password button to login page

--- api/views/login.php
+++ api/views/login.php
@@ -9,6 +9,9 @@
 			<input type="password" id="login-password" class="p-2.5 rounded-lg" required />
 		</div>
 		<button type="submit" class="bg-color-secondary rounded-lg w-auto px-5 py-2.5">Submit</button>
+		<button id="forgot-password" class="bg-color-secondary rounded-lg px-2.5 py-1 w-full my-2.5">
+			I forgot my password
+		</button>
 	</form>
 	<section id="form-message"></section>
 </section>
--- api/views/settings.php
+++ api/views/settings.php
@@ -22,9 +22,6 @@
                 Update
             </button>
         </div>
-        <button id="forgot-password" class="bg-color-secondary rounded-lg px-5 py-2.5 w-full my-2">
-            I forgot my password
-        </button>
         <label class="my-2 inline-flex items-center cursor-pointer w-full">
             <input id="email-checkbox" type="checkbox" value="" class="sr-only peer">
             <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
