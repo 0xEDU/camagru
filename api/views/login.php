9cccdd4a41a1eec6a644bcde9fc846f62a2f13da
edu <bfwaend@gmail.com>
2025-02-10 23:41:15 -0300

    Update login layout

--- api/views/login.php
+++ api/views/login.php
@@ -1,17 +1,19 @@
 <section class="font-mono flex flex-col items-center justify-center w-full">
-	<form id="login-form" class="flex flex-col items-center">
-		<div class="mb-5">
-			<label for="login-username" class="block">Your username</label>
-			<input type="text" id="login-username" class="p-2.5 rounded-lg" placeholder="user" required />
-		</div>
-		<div class="mb-5">
-			<label for="login-password" class="block">Your password</label>
-			<input type="password" id="login-password" class="p-2.5 rounded-lg" required />
-		</div>
-		<button type="submit" class="bg-color-secondary rounded-lg w-auto px-5 py-2.5">Submit</button>
-		<button id="forgot-password" class="bg-color-secondary rounded-lg px-2.5 py-1 w-full my-2.5">
+	<div class="flex flex-col items-center">
+		<form id="login-form" class="flex flex-col items-center">
+			<div class="mb-5">
+				<label for="login-username" class="block">Your username</label>
+				<input type="text" id="login-username" class="p-2.5 rounded-lg" placeholder="user" required />
+			</div>
+			<div class="mb-5">
+				<label for="login-password" class="block">Your password</label>
+				<input type="password" id="login-password" class="p-2.5 rounded-lg" required />
+			</div>
+			<button type="submit" class="bg-color-secondary rounded-lg w-auto px-5 py-2.5">Submit</button>
+		</form>
+		<section id="form-message"></section>
+		<button id="forgot-password-button" class="bg-color-secondary rounded-lg px-2.5 py-1 w-full my-2.5">
 			I forgot my password
 		</button>
-	</form>
-	<section id="form-message"></section>
+	</div>
 </section>
